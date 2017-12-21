66802a535111b153ee22824308e7f009455eae6b
andrea <andrea.peruffo1982@gmail.com>
2017-12-21 18:41:14 +0000

    more from the plane

--- shared/src/main/scala/com/typesafe/config/ConfigFactory.scala
+++ shared/src/main/scala/com/typesafe/config/ConfigFactory.scala
@@ -5,6 +5,9 @@ import scala.collection.mutable
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox.Context
 
+import java.{time => jt, util => ju}
+import java.util.{concurrent => juc}
+
 object MacroUtils {
 
   // I'm relying on the fact that the compiler is single thread
@@ -12,8 +15,6 @@ object MacroUtils {
 
   def extractString(c: Context)(str: c.Expr[String]): String = {
     import c.universe._
-    import scala.reflect.runtime.universe.{ reify, showRaw, show }
-    println( show( str ) )
 
     str.tree match {
       case q"""$strLit""" =>
@@ -33,12 +34,12 @@ object MacroUtils {
 object Config {
   val configStrings = mutable.Map[Int, String]()
 
+  def index = MacroUtils.i
+
+  def config = ct.config.ConfigFactory.parseString(configStrings(index))
+
   def getStringImpl(c: Context)(path: c.Expr[String]): c.Expr[String] = {
     import c.universe._
-
-    val index = MacroUtils.i
-
-    val config = ct.config.ConfigFactory.parseString(configStrings(index))
 
     val pathStr = MacroUtils.extractString(c)(path)
 
@@ -46,12 +47,152 @@ object Config {
 
     c.Expr[String](q"""$res""")
   }
+
+  def getIntImpl(c: Context)(path: c.Expr[String]): c.Expr[Int] = {
+    import c.universe._
+
+    val pathStr = MacroUtils.extractString(c)(path)
+
+    val res = config.getInt(pathStr)
+
+    c.Expr[Int](q"""$res""")
+  }
+
+
+  def getBooleanImpl(c: Context)(path: c.Expr[String]): c.Expr[Boolean] = {
+    import c.universe._
+
+    val pathStr = MacroUtils.extractString(c)(path)
+
+    val res = config.getBoolean(pathStr)
+
+    c.Expr[Boolean](q"""$res""")
+  }
+
+  def getLongImpl(c: Context)(path: c.Expr[String]): c.Expr[Long] = {
+    import c.universe._
+
+    val pathStr = MacroUtils.extractString(c)(path)
+
+    val res = config.getLong(pathStr)
+
+    c.Expr[Long](q"""$res""")
+  }
+
+  def getDoubleImpl(c: Context)(path: c.Expr[String]): c.Expr[Double] = {
+    import c.universe._
+
+    val pathStr = MacroUtils.extractString(c)(path)
+
+    val res = config.getDouble(pathStr)
+
+    c.Expr[Double](q"""$res""")
+  }
+
+  def hasPathImpl(c: Context)(path: c.Expr[String]): c.Expr[Boolean] = {
+    import c.universe._
+
+    val pathStr = MacroUtils.extractString(c)(path)
+
+    val res = config.hasPath(pathStr)
+
+    c.Expr[Boolean](q"""$res""")
+  }
+
+  def getStringListImpl(c: Context)(path: c.Expr[String]): c.Expr[ju.List[String]] = {
+    import c.universe._
+    import scala.collection.JavaConverters._
+
+    val pathStr = MacroUtils.extractString(c)(path)
+
+    val res = config.getStringList(pathStr).asScala
+
+    // this could be better probably ...
+    c.Expr[ju.List[String]](q"""{
+      import scala.collection.JavaConverters._
+      List(..$res).asJava
+    }""")
+  }
+
+  def getDurationImpl(c: Context)(path: c.Expr[String]): c.Expr[jt.Duration] = {
+    import c.universe._
+
+    val pathStr = MacroUtils.extractString(c)(path)
+
+    val res = config.getDuration(pathStr).toNanos
+
+    c.Expr[jt.Duration](q"""{
+      java.time.Duration.ofNanos($res)
+    }""")
+  }
+
+  // def getDurationTUImpl(c: Context)(path: c.Expr[String], tu: c.Expr[juc.TimeUnit]): c.Expr[Long] = {
+  //   import c.universe._
+  //
+  //   val pathStr = MacroUtils.extractString(c)(path)
+  //
+  //   val res = config.getDuration(pathStr)
+  //
+  //   c.Expr[Long](q"""{
+  //     $res.get($tu).toNanos
+  //   }""")
+  // }
+
+  def getConfigImpl(c: Context)(path: c.Expr[String]): c.Expr[Config] = {
+    import c.universe._
+
+    val pathStr = MacroUtils.extractString(c)(path)
+
+    val configStr = config.getConfig(pathStr).root.render
+
+    MacroUtils.i += 1
+
+    Config.configStrings.update(MacroUtils.i, configStr)
+
+    c.Expr[Config](q"""{
+        new com.typesafe.config.Config()
+      }""")
+  }
+
+  // def withFallbackImpl(c: Context)(config: c.Expr[Config]): c.Expr[Config] = {
+  //   import c.universe._
+  //
+  //   val configStr = Config.config.root.render ++ q"""$config.root.render"""
+  //
+  //   MacroUtils.i += 1
+  //
+  //   Config.configStrings.update(MacroUtils.i, configStr)
+  //
+  //   c.Expr[Config](q"""{
+  //       new com.typesafe.config.Config()
+  //     }""")
+  // }
+
 }
 
 class Config() {
 
+  def hasPath(path: String): Boolean = macro Config.hasPathImpl
+
   def getString(path: String): String = macro Config.getStringImpl
 
+  def getBoolean(path: String): Boolean = macro Config.getBooleanImpl
+
+  def getInt(path: String): Int = macro Config.getIntImpl
+
+  def getLong(path: String): Long = macro Config.getLongImpl
+
+  def getDouble(path: String): Double = macro Config.getDoubleImpl
+
+  def getStringList(path: String): ju.List[String] = macro Config.getStringListImpl
+
+  def getDuration(path: String): jt.Duration = macro Config.getDurationImpl
+
+  // def getDuration(path: String, unit: juc.TimeUnit): Long = macro Config.getDurationTUImpl
+
+  def getConfig(path: String): Config = macro Config.getConfigImpl
+
+  // def withFallback(config: Config): Config = macro Config.withFallbackImpl
 }
 
 
@@ -66,7 +207,7 @@ object ConfigFactory {
 
     Config.configStrings.update(MacroUtils.i, configStr)
 
-    println("configStr "+configStr)
+    // println("configStr "+configStr)
 
     val res =
       c.Expr[com.typesafe.config.Config](q"""{
--- shared/src/test/scala/ConfigBaseSpec.scala
+++ shared/src/test/scala/ConfigBaseSpec.scala
@@ -20,18 +20,165 @@ import org.junit._
 import scala.collection.JavaConverters._
 import java.util.concurrent.TimeUnit
 
+import com.typesafe.config.ConfigFactory
+
 class ConfigBaseSpec {
 
   @Test
   def basicTestConfig2() = {
-    val config = com.typesafe.config.ConfigFactory.parseString("""{ "a" : "2" }""")
-    println("2 -> a is: " + config.getString("a"))
+    val config = ConfigFactory.parseString("""{ "a" : "2" }""")
+    assertEquals(config.getString("a"), "2")
   }
 
   @Test
   def basicTestConfig3() = {
-    val config = com.typesafe.config.ConfigFactory.parseString("""{ "a" : "3" }""")
-    println("3 -> a is: " + config.getString("a"))
+    val config = ConfigFactory.parseString("""{ "a" : "3" }""")
+    assertEquals(config.getString("a"), "3")
   }
 
+  @Test
+  def parseEmptyList() = {
+    val config = ConfigFactory.parseString("""{ "a" : [] }""")
+
+    assert { config != null }
+    assert { config.hasPath("a") }
+
+    assert { config.getStringList("a").isEmpty }
+  }
+
+  @Test
+  def parseBasicValues() = {
+    val config = ConfigFactory.parseString("""{ "a" : "2" }""")
+
+    assert { config != null }
+
+    assertEquals(config.getString("a"), "2")
+    assertEquals(config.getInt("a"), 2)
+  }
+
+  @Test
+  def parseStringLiteralsWithSlashes() = {
+    val config = ConfigFactory.parseString("""a = some/path""")
+    assert { config != null }
+    assertEquals(config.getString("a"), "some/path")
+  }
+
+  @Test
+  def parseLists() = {
+    val config1 =
+      ConfigFactory.parseString(
+      """l =[ a
+
+           b
+          c
+
+         d ]"""
+    )
+
+    assert { config1 != null }
+
+    assertEquals(config1.getStringList("l"), List("a", "b", "c", "d").asJava)
+    // assertEquals(config2.getStringList("l"), config1.getStringList("l"))
+  }
+
+  @Test
+  def parseNestedObjects() = {
+    val config = ConfigFactory.parseString("a = { b = 1 }")
+
+    assert { config != null }
+
+    assertEquals(config.getConfig("a").getInt("b"), 1)
+  }
+
+  @Test
+  def pasreNewLinesIsteadOfCommas = {
+    val config = ConfigFactory.parseString("""{
+    foo = 1
+
+    bar = 2
+
+    baz = 3}
+    """)
+
+    assert { config != null }
+
+    assertEquals(config.getInt("foo"), 1)
+    assertEquals(config.getInt("bar"), 2)
+    assertEquals(config.getInt("baz"), 3)
+  }
+
+  // @Test
+  // def parseConcatenatedValues = {
+  //   val config1 = ConfigFactory.parseString("x = {a:1, b: 2}\n {c: 3, d: 4}")
+  //
+  //   val config2 = ConfigFactory.parseString("x = {a:1, b: 2\nc: 3, d: 4}")
+  //
+  //   assert { config1 != null && config2 != null }
+  //
+  //   assertEquals(config1, config2)
+  // }
+
+  @Test
+  def parseAndConcatenateStringValues = {
+    val config = ConfigFactory.parseString("""
+        x = a b c d
+        y = 10
+        """)
+
+    assert { config != null }
+
+    assertEquals(config.getString("x"), "a b c d")
+  }
+
+  @Test
+  def parseDurations = {
+    val config = ConfigFactory.parseString(
+      """ a {
+        x = 1 ms
+        }"""
+    )
+
+    assert { config != null }
+
+    assertEquals(config.getDuration("a.x").toMillis.toLong, 1L)
+    // assertEquals(config.getDuration("a.x", TimeUnit.NANOSECONDS).toLong,
+    //              1000000L)
+  }
+
+  @Test
+  def parseBooleans = {
+    val config = ConfigFactory.parseString(
+      """ a {
+        x1 = true
+        x2 = on
+        x3 = yes
+        x4 = false
+        x5 = off
+        x6 = no
+        }"""
+    )
+
+    assert { config != null }
+
+    assertEquals(config.getBoolean("a.x1"), true)
+    assertEquals(config.getBoolean("a.x2"), true)
+    assertEquals(config.getBoolean("a.x3"), true)
+    assertEquals(config.getBoolean("a.x4"), false)
+    assertEquals(config.getBoolean("a.x5"), false)
+    assertEquals(config.getBoolean("a.x6"), false)
+  }
+
+  // @Test
+  // def reloadConfigWithFallback() = {
+  //   val config1 = ConfigFactory.parseString("""{ "a" : [] }""")
+  //   val config2 = ConfigFactory.parseString("""{ "b" : [] }""")
+  //
+  //   assert(config1 != null && config2 != null, "both config were null")
+  //
+  //   val config = config1.withFallback(config2).withFallback(config1)
+  //
+  //   assert(config.hasPath("a"), "config must have path a")
+  //   assert(config.hasPath("b"), "config must have path b")
+  //
+  // }
 }
